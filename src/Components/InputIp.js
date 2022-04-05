1ba766cfa4d9247b0406f6b5a09b35cabdb09410
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-04-05 22:15:15 +0200

    first commit and push to github

---
+++ src/Components/InputIp.js
@@ -0,0 +1,12 @@
+function InputIp (props){
+
+    return <form className="address">
+                <h2>IP Address Tracker</h2>
+                <input placeholder="1.1.1.1" />
+                <button>
+                    <img alt="button-arrow-icon" src="/images/icon-arrow.svg" />
+                </button>
+           </form>
+}
+
+export default InputIp;
