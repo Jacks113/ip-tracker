d70a246c0ba8a60cd2de50da3e83956ac8cd62e5
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-04-08 08:08:45 +0200

    ip fetch update

--- src/Services/getIP.js
+++ src/Services/getIP.js
@@ -15,19 +15,22 @@ function getIP (setIp, setIpError, ipAddress){
 }
 
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com',
-		'X-RapidAPI-Key': '12fd189607msh5aab80fadacaf12p129a74jsne25b07624d52'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '12fd189607msh5aab80fadacaf12p129a74jsne25b07624d52'
+// 	}
+// };
 
 
 
-function getGeoData(){
+function getGeoData(loc){
 
-    fetch('https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=Grad%20Zagreb&language=en', options)
+    let address = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + loc.toString() + ".json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoiamFzMTIiLCJhIjoiY2wxb3N1bDdkMDJzdzNrcXV1aWEwNThybSJ9.tqNy9WQdPHVV5Kocma2QJw";
+
+
+    fetch(address)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
